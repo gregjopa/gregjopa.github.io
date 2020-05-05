186e9eb15ff42b4ceca2fd787763ddd6cced43dd
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2020-05-05 15:04:31 -0500

    Add new alert component

---
+++ src/components/Alert.js
@@ -0,0 +1,17 @@
+/** @jsx jsx */
+import { jsx, Container } from "theme-ui"
+
+function Alert ({ children }) {
+  return (
+    <Container
+      sx={{
+        border: "1px solid",
+        borderColor: "secondary",
+        fontSize: [1, 2]
+      }}>
+        {children}
+    </Container>
+  )
+}
+
+export default Alert
