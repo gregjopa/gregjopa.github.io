d8995439392e867a22390abbd63c7655e5fdefae
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2023-01-16 14:29:21 -0600

    chore: update gatsby and minimal blog theme

---
+++ gatsby-config.ts
@@ -0,0 +1,179 @@
+import type { GatsbyConfig, PluginRef } from "gatsby"
+import "dotenv/config"
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    siteTitle: `Greg Jopa`,
+    siteTitleAlt: `Greg Jopa - Adventures in Web Development`,
+    siteHeadline: `Greg Jopa - Adventures in Web Development`,
+    siteUrl: `https://www.gregjopa.com/`,
+    siteDescription: `Web development blog of Greg Jopa. Featuring articles about JavaScript and other interesting web technologies.`,
+    siteLanguage: `en`,
+    siteImage: `/greg_jopa_outdoor_headshot.jpg`,
+    author: `@gregjopa`
+  },
+  trailingSlash: `never`,
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      // See the theme's README for all available options
+      options: {
+        formatString: `MM.DD.YYYY`,
+        navigation: [
+          {
+            title: `Blog`,
+            slug: `/blog`,
+          },
+          {
+            title: `About`,
+            slug: `/about`,
+          },
+        ],
+        externalLinks: [
+          {
+            name: `GitHub`,
+            url: `https://github.com/gregjopa/`
+          },
+          {
+            name: `Twitter`,
+            url: `https://twitter.com/gregjopa/`
+          },
+          {
+            name: `LinkedIn`,
+            url: `https://www.linkedin.com/in/gregjopa/`
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        output: `/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gregjopa.com - Adventures in Web Development`,
+        short_name: `gregjopa.com`,
+        description: `Web development blog of Greg Jopa. Featuring articles about JavaScript and other interesting web technologies.`,
+        start_url: `/`,
+        background_color: `#fff`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        theme_color: `#6B46C1`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title: siteTitle
+                description: siteDescription
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({
+              query: { site, allPost },
+            }: {
+              query: { allPost: IAllPost; site: { siteMetadata: ISiteMetadata } }
+            }) =>
+              allPost.nodes.map((post) => {
+                const url = site.siteMetadata.siteUrl + post.slug
+                const content = `<p>${post.excerpt}</p><div style="margin-top: 50px; font-style: italic;"><strong><a href="${url}">Keep reading</a>.</strong></div><br /> <br />`
+
+                return {
+                  title: post.title,
+                  date: post.date,
+                  excerpt: post.excerpt,
+                  url,
+                  guid: url,
+                  custom_elements: [{ "content:encoded": content }],
+                }
+              }),
+            query: `{
+  allPost(sort: {date: DESC}) {
+    nodes {
+      title
+      date(formatString: "MMMM D, YYYY")
+      excerpt
+      slug
+    }
+  }
+}`,
+            output: `rss.xml`,
+            title: `Minimal Blog - @lekoarts/gatsby-theme-minimal-blog`,
+          },
+        ],
+      },
+    },
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  ].filter(Boolean) as Array<PluginRef>,
+}
+
+export default config
+
+interface IPostTag {
+  name: string
+  slug: string
+}
+
+interface IPost {
+  slug: string
+  title: string
+  defer: boolean
+  date: string
+  excerpt: string
+  contentFilePath: string
+  html: string
+  timeToRead: number
+  wordCount: number
+  tags: Array<IPostTag>
+  banner: any
+  description: string
+  canonicalUrl: string
+}
+
+interface IAllPost {
+  nodes: Array<IPost>
+}
+
+interface ISiteMetadata {
+  siteTitle: string
+  siteTitleAlt: string
+  siteHeadline: string
+  siteUrl: string
+  siteDescription: string
+  siteImage: string
+  author: string
+}
--- src/@lekoarts/gatsby-theme-minimal-blog/components/post.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/post.tsx
@@ -1,48 +1,43 @@
 /** @jsx jsx */
-import { jsx, Styled } from "theme-ui"
-import { MDXRenderer } from "gatsby-plugin-mdx"
-import React from "react"
+import type { HeadFC, PageProps } from "gatsby"
+import * as React from "react"
+import { jsx, Heading } from "theme-ui"
 import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
 import ItemTags from "@lekoarts/gatsby-theme-minimal-blog/src/components/item-tags"
-import SEO from "@lekoarts/gatsby-theme-minimal-blog/src/components/seo"
+import Seo from "@lekoarts/gatsby-theme-minimal-blog/src/components/seo"
+import PostFooter from "@lekoarts/gatsby-theme-minimal-blog/src/components/post-footer"
 
-type PostProps = {
-  data: {
-    post: {
+export type MBPostProps = {
+  post: {
+    slug: string
+    title: string
+    date: string
+    tags?: {
+      name: string
       slug: string
-      title: string
-      date: string
-      tags?: {
-        name: string
-        slug: string
-      }[]
-      description?: string
-      body: string
-      excerpt: string
-      timeToRead?: number
-      banner?: {
-        childImageSharp: {
-          resize: {
-            src: string
-          }
+    }[]
+    description?: string
+    canonicalUrl?: string
+    excerpt: string
+    timeToRead?: number
+    banner?: {
+      childImageSharp: {
+        resize: {
+          src: string
         }
       }
     }
   }
 }
 
-const px = [`32px`, `16px`, `8px`, `4px`]
-const shadow = px.map((v) => `rgba(0, 0, 0, 0.15) 0px ${v} ${v} 0px`)
+const px = [`16px`, `8px`, `4px`]
+const shadow = px.map((v) => `rgba(0, 0, 0, 0.1) 0px ${v} ${v} 0px`)
 
-const Post = ({ data: { post } }: PostProps) => (
+const Post: React.FC<React.PropsWithChildren<PageProps<MBPostProps>>> = ({ data: { post }, children }) => (
   <Layout>
-    <SEO
-      title={post.title}
-      description={post.description ? post.description : post.excerpt}
-      image={post.banner ? post.banner.childImageSharp.resize.src : undefined}
-      pathname={post.slug}
-    />
-    <Styled.h2>{post.title}</Styled.h2>
+    <Heading as="h1" variant="styles.h1">
+      {post.title}
+    </Heading>
     <p sx={{ color: `secondary`, mt: 3, a: { color: `secondary` }, fontSize: [1, 1, 2] }}>
       <time>{post.date}</time>
       {post.tags && (
@@ -52,10 +47,34 @@ const Post = ({ data: { post } }: PostProps) => (
         </React.Fragment>
       )}
     </p>
-    <section sx={{ my: 5, ".gatsby-resp-image-wrapper": { my: [4, 4, 5], boxShadow: shadow.join(`, `) } }}>
-      <MDXRenderer>{post.body}</MDXRenderer>
+    <section
+      sx={{
+        my: 5,
+        ".gatsby-resp-image-wrapper": {
+          my: [4, 4, 5],
+          borderRadius: `4px`,
+          boxShadow: shadow.join(`, `),
+          ".gatsby-resp-image-image": {
+            borderRadius: `4px`,
+          },
+        },
+        variant: `layout.content`,
+      }}
+    >
+      {children}
     </section>
+    <PostFooter post={post} />
   </Layout>
 )
 
 export default Post
+
+export const Head: HeadFC<MBPostProps> = ({ data: { post } }) => (
+  <Seo
+    title={post.title}
+    description={post.description ? post.description : post.excerpt}
+    image={post.banner ? post.banner?.childImageSharp?.resize?.src : undefined}
+    pathname={post.slug}
+    canonicalUrl={post.canonicalUrl}
+  />
+)
