6e233ca743d5b97d646e0c5a27b9f6a8e916cfc9
gregjopa <gregjopa@users.noreply.github.com>
2023-03-22 22:12:43 +0000

    deployed via Gatsby Publish Action 🎩 for 4e0072e9322766615f03e31225a2d0fdf3879dc5

---
+++ component---src-pages-404-tsx-c05340b2a095bec28227.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgregjopa_github_io=self.webpackChunkgregjopa_github_io||[]).push([[218],{2513:function(e,t,n){n.r(t);var l=n(7294),u=n(6920);t.default=e=>l.createElement(u.Z,null,l.createElement("h1",null,"404 - Page Not Found"),l.createElement("p",null,"Unfortunately we couldn't find what you were looking for :("))}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-c05340b2a095bec28227.js.map
